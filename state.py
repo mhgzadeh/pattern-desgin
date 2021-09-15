9c3e87a5c83e96b8702902597d0b36fa35a48eb5
Mohammad Hosseingholizadeh <m.hgzahdeh@gmail.com>
2021-09-16 00:06:43 +0200

    design pattern - Behavorial State

---
+++ state.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+
+
+class Message:
+    def __init__(self, subject, body, sender):
+        self.subject = subject
+        self.body = body
+        self.sender = sender
+        self.flow = [sender]
+
+    @property
+    def current(self):
+        return self.flow[-1]
+
+    def send(self, to_user):
+        if to_user.__class__ not in self.current.allowed:
+            print(f"{self.current.__class__} is not allowed to send email to {to_user.__class__}")
+        else:
+            self.flow.append(to_user)
+            print(f"message send to {to_user.__class__}")
+
+
+class AbsractUser(ABC):
+    @property
+    @abstractmethod
+    def allowed(self):
+        pass
+
+
+class ManagingDirector(AbsractUser):
+    allowed = []
+
+
+class InternalManager(AbsractUser):
+    allowed = [ManagingDirector]
+
+
+class Supervisor(AbsractUser):
+    allowed = [InternalManager]
+
+
+class Operator(AbsractUser):
+    allowed = [Supervisor]
+
+
+class Client(AbsractUser):
+    allowed = [Operator]
+
+
+if __name__ == "__main__":
+    opt = Operator()
+    spr = Supervisor()
+    inm = InternalManager()
+    mnd = ManagingDirector()
+
+    client = Client()
+    message = Message("Issue #1234", "Issue description", client)
+
+    message.send(opt)
+    message.send(spr)
+    message.send(inm)
+    message.send(mnd)
